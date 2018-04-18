004fad06c634017d5c31a8180017d36344f30b4a
brandojc <brandojc@rose-hulman.edu>
2018-04-17 22:07:00 -0400

    Done

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  April 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and James Brandon.  April 2018.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -12,7 +12,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the code of the  Rect  class below.
+# Done: 2.  READ the code of the  Rect  class below.
 #
 #   Once you are confident that you understand the  Rect  class and its code,
 #   change the TO-DO for this problem to DONE.
@@ -29,12 +29,36 @@ class Rect(object):
 def run_test_problem1():
     """ Tests the   problem1   function. """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement at least 2 tests of the  problem1  function.
+    # Done: 3. Implement at least 2 tests of the  problem1  function.
     # -------------------------------------------------------------------------
     print()
     print('--------------------------------------------------')
     print('Testing the   problem1   function:')
     print('--------------------------------------------------')
+
+    print('Test 1:')
+
+    expected = 762
+    actual = problem1([Rect(5, 10), Rect(4, 3), Rect(100, 7)])
+
+    print()
+    print('Expected =', expected)
+    print('Actual =', actual)
+    if actual == expected:
+        print('True')
+    print()
+
+    print('Test 2:')
+
+    expected = 140
+    actual = problem1([Rect(10, 10), Rect(5, 5), Rect(3, 3), Rect(2, 3)])
+
+    print()
+    print('Expected =', expected)
+    print('Actual =', actual)
+    if actual == expected:
+        print('True')
+    print()
 
 
 def problem1(rectangles):
@@ -53,9 +77,13 @@ def problem1(rectangles):
     :return: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     # -------------------------------------------------------------------------
 
+    total_area = 0
+    for k in range(len(rectangles)):
+        total_area = total_area + (rectangles[k].w * rectangles[k].h)
+    return total_area
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
